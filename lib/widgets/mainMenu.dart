67a39fc569be6013bc4e1e30b6a6ce7592c9b6dd
Sergio Madrid Valiente <SMADVAL@upv.edu.es>
2023-10-07 17:18:27 +0200

    Checkpoint

--- lib/widgets/mainMenu.dart
+++ lib/widgets/mainMenu.dart
@@ -1,51 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:metrovalencia/widgets/coheteGrande.dart';
 
-Widget mainMenu(BuildContext context) {
-  return Container(
-    width: MediaQuery.of(context).size.width,
-    decoration: const BoxDecoration(
-      image: DecorationImage(
-        image: AssetImage('assets/1.jpg'), // Replace with your image asset path
-        fit: BoxFit.cover, // You can adjust the fit mode as needed
+class mainMenu extends StatefulWidget {
+  const mainMenu({super.key});
+
+  @override
+  State<mainMenu> createState() => _mainMenuState();
+}
+
+class _mainMenuState extends State<mainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image:
+              AssetImage('assets/1.jpg'), // Replace with your image asset path
+          fit: BoxFit.cover, // You can adjust the fit mode as needed
+        ),
       ),
-    ),
-    child: Expanded(
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 100,
-          ),
-          Stack(
-            alignment: Alignment.topCenter,
-            children: [
-              SizedBox(
-                height: 500,
-                child: Hero(
-                  tag: 'imageTierra',
-                  child: Image.asset('assets/tierra.png'),
+      child: Expanded(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                SizedBox(
+                  height: 500,
+                  child: Hero(
+                    tag: 'imageTierra',
+                    child: Image.asset('assets/tierra.png'),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 150,
-                child: Hero(
-                  tag: 'imageCohete',
-                  child: Image.asset('assets/cohete.png'),
-                ),
-              )
-            ],
-          ),
-          ElevatedButton(
-            child: Text("siguietne"),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => coheteGrande(context)));
-            },
-          )
-        ],
+                SizedBox(
+                  height: 150,
+                  child: Hero(
+                    tag: 'imageCohete',
+                    child: Image.asset('assets/cohete.png'),
+                  ),
+                )
+              ],
+            ),
+            ElevatedButton(
+              child: Text("siguietne"),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => coheteGrande(context)));
+              },
+            )
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
